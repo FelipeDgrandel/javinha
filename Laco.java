15e8c9dee0ab358eafd71cc2d27877be3026e4fc
Felipe <felipe.veigarpm@gmail.com>
2024-03-23 13:04:19 -0300

    22-3

--- Laco.java
+++ Laco.java
@@ -1,5 +1,8 @@
 public class Laco {
-    public static void main(String[] args) {
-        
+    // imprimaos pares de 20 ate 2 nessa ordem.
+    public static void mai(String[] args) {
+        for(int i = 20; i >= 2; i -= 2){
+            System.out.println(i);
+        }
     }
 }
